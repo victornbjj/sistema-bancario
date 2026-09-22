e208867832f9e2915b845f1f266a66bf75be98d0
Seu Nome Completo <victorifpb78@gmail.com>
2026-09-22 15:21:06 -0300

    feat: adicioanr DTO de extrato para movimentação do meetodo GET

---
+++ src/main/java/br/com/sistemabancario/api/dto/ExtratoItemResponse.java
@@ -0,0 +1,21 @@
+package br.com.sistemabancario.api.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import br.com.sistemabancario.api.enums.TipoTransacao;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExtratoItemResponse {
+    private Long id;
+    private TipoTransacao tipo;
+    private BigDecimal valor;
+    private LocalDateTime data;
+}
