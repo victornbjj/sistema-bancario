754be12ddb61d405ea36d6118eaff8afc0845770
Seu Nome Completo <victorifpb78@gmail.com>
2026-09-22 11:46:03 -0300

    feat: adiciona regra de negocio pra rendimentos e juros

--- src/main/java/br/com/sistemabancario/api/database/repository/ITransacaoRepository.java
+++ src/main/java/br/com/sistemabancario/api/database/repository/ITransacaoRepository.java
@@ -1,7 +1,13 @@
 package br.com.sistemabancario.api.database.repository;
 
 import br.com.sistemabancario.api.database.entity.TransacaoEntity;
+import br.com.sistemabancario.api.enums.TipoTransacao;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITransacaoRepository extends JpaRepository<TransacaoEntity, Long> {
+   
+    Optional<TransacaoEntity> findTopByContaIdAndTipoOrderByDataDesc (Long contaId, TipoTransacao tipo);
 }
